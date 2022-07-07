2d7129c1b926da663cf6e28766770d1f8dfc2cbe
flportilla <flportilla@gmail.com>
2022-07-07 07:29:32 -0500

    Add backend files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import Note from './components/Note'
 import { useEffect, useState } from 'react'
-import noteService from '../src/services/notes'
+import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+
 
 function App() {
 
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
 
   const getNotes = () => {
     noteService
       .getAll()
-      .then(initialNotes => setNotes(initialNotes))
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
   };
 
   useEffect(getNotes, [])
@@ -27,9 +34,12 @@ function App() {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note ${note.content} was already deleted from the server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(note => note.id !== id))
       })
   }
@@ -42,6 +52,7 @@ function App() {
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
     }
+
     noteService
       .create(noteObject)
       .then(returnedNote =>
@@ -61,6 +72,8 @@ function App() {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+
       <div>
         <button type="button" onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -79,6 +92,7 @@ function App() {
         />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
