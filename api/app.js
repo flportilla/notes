347c5c6fe5d28ccfe0e3ac1598b89fc07f35b873
flportilla <flportilla@gmail.com>
2022-07-08 16:55:03 -0500

    Add login controller and library

--- api/app.js
+++ api/app.js
@@ -1,13 +1,19 @@
-const config = require('./utils/config')
+
 const express = require('express')
-require('express-async-errors')
-const app = express()
+const mongoose = require('mongoose')
 const cors = require('cors')
+
+require('express-async-errors')
+
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+
+const app = express()
 
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -23,11 +29,12 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
+app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
