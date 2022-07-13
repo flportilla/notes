3928ff717b2f8286dabcd51f638150fd10197e16
flportilla <flportilla@gmail.com>
2022-07-13 08:01:39 -0500

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import noteService from './services/notes'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
+import React from 'react'
 
 
 function App() {
